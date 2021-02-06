00c3663e056462d30f49666349b57550ccf60cc6
Pavlov Vitalii <pavlov-vv86@yandex.ru>
2021-02-07 01:28:46 +0300

    Создание компонент для страниц

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -102,7 +102,7 @@ const MainPage = (props)=> {
           <div className="catalog__movies-list">
 
             {
-              [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map((element, i)=><MovieCard key={i}></MovieCard>)
+              [0, 1, 2, 3, 4, 5, 6, 7].map((element, i)=><MovieCard key={i}></MovieCard>)
             }
 
           </div>
---
+++ src/components/not-found-page/not-found-page.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <div className="user-page">
+  <header className="page-header user-page__head">
+    <div className="logo">
+      <a href="main.html" className="logo__link">
+        <span className="logo__letter logo__letter--1">W</span>
+        <span className="logo__letter logo__letter--2">T</span>
+        <span className="logo__letter logo__letter--3">W</span>
+      </a>
+    </div>
+    <h1 className="page-title user-page__title">Error</h1>
+  </header>
+  <div className="sign-in user-page__content">
+    <h2>404. Page not found.</h2>
+    <link to="/" />Вернуться на главную
+  </div>
+  <footer className="page-footer">
+    <div className="logo">
+      <a href="main.html" className="logo__link logo__link--light">
+        <span className="logo__letter logo__letter--1">W</span>
+        <span className="logo__letter logo__letter--2">T</span>
+        <span className="logo__letter logo__letter--3">W</span>
+      </a>
+    </div>
+    <div className="copyright">
+      <p>© 2019 What to watch Ltd.</p>
+    </div>
+  </footer>
+</div>
+
+  )
+};
+
+export default NotFoundPage;
