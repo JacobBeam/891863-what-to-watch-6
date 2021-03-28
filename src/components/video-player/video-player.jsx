08732707cdbb6f6e35b40770d45091c9988cdb08
Pavlov Vitalii <pavlov-vv86@yandex.ru>
2021-03-28 23:41:06 +0300

    Загрузка видео при наведении и по таймауту

--- src/components/video-player/video-player.jsx
+++ src/components/video-player/video-player.jsx
@@ -14,12 +14,14 @@ const VideoPlayer = (props)=> {
 
     videoRef.current.onmouseenter = () => {
       timer = setTimeout(() => {
+        videoRef.current.src = previewVideoLink;
         videoRef.current.play();
       }, TIMEOUT_IN_MS);
     };
 
     videoRef.current.onmouseleave = () => {
       clearTimeout(timer);
+      videoRef.current.src = ``;
       videoRef.current.load();
     };
 
@@ -27,6 +29,7 @@ const VideoPlayer = (props)=> {
       clearTimeout(timer);
       videoRef.current.onmouseenter = null;
       videoRef.current.onmouseleave = null;
+      videoRef.current.src = ``;
     };
 
   }, []);
@@ -34,7 +37,6 @@ const VideoPlayer = (props)=> {
   return (
     <Fragment>
       <video
-        src={previewVideoLink}
         ref={videoRef}
         width="280"
         height="175"
